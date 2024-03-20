1f379009a7be0a2a6047efa2807b1eaf06ad4b8f
Alan Zhan <55457750+Sybaru@users.noreply.github.com>
2024-03-19 21:26:17 -0400

    Update books_status.js

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,10 @@
 let BookInstance = require('../models/bookinstance');
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+exports.show_all_books_status = async (res) => {
+  try {
+    const books = await Book.find({}, { title: 1, available: 1 });
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch book status' });
+  }
+};
